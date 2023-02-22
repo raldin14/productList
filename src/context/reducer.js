2ae437d0689758982704cca10d91ab3a87b4dcf4
Raldin <raldin.hidalgo@gmail.com>
2023-02-22 00:55:49 -0400

    Formating document

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,23 +1,27 @@
 const reducer = (state, action) => {
-    switch (action.type) {
-      case "SEARCH_PRODUCT_REQUEST":
-        return {
-          ...state,
-          loading: true,
-        };
-      case "SEARCH_PRODUCT_SUCCESS":
-        return {
-          ...state,
-          loading: false,
-          products: action.payload,
-        };
+  switch (action.type) {
+    case "SEARCH_PRODUCT_REQUEST":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "SEARCH_PRODUCT_SUCCESS":
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
     case "FILTER_PRODUCT_REQUEST":
-    return  {...state,
-        loading: false,products: action.payload.productList.filter((s) =>
-    s.title.toLowerCase().includes(action.payload.query.toLowerCase()))};
-      default:
-        return state;
-    }
-  };
+      return {
+        ...state,
+        loading: false,
+        products: action.payload.productList.filter((s) =>
+          s.title.toLowerCase().includes(action.payload.query.toLowerCase())
+        ),
+      };
+    default:
+      return state;
+  }
+};
 
-  export default reducer;
+export default reducer;
